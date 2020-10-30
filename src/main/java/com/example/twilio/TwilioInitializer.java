d11c475768620082b0013168ff52c153d7b55e19
isakulaksiz <isakulaksiz@outlook.com>
2020-10-30 18:25:08 +0300

    fix typo Dependency Injection

--- src/main/java/com/example/twilio/TwilioInitializer.java
+++ src/main/java/com/example/twilio/TwilioInitializer.java
@@ -1,7 +1,16 @@
 package com.example.twilio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TwilioInitializer {
+
+    private final TwilioConfig twilioConfig;
+
+    @Autowired
+    private TwilioInitializer(TwilioConfig twilioConfig){
+        this.twilioConfig=twilioConfig;
+    }
+
 }
