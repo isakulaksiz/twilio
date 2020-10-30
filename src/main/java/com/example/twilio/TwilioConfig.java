7c5437d863fdd147d38a96e16338bb1266272518
isakulaksiz <isakulaksiz@outlook.com>
2020-10-30 18:16:18 +0300

    Added twilio dependencies

---
+++ src/main/java/com/example/twilio/TwilioConfig.java
@@ -0,0 +1,10 @@
+package com.example.twilio;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("twilio")
+public class TwilioConfig {
+
+}
