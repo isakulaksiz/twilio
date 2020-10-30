31084dbb02bc8034670da6ca14d3ce12ce8f7e76
isakulaksiz <isakulaksiz@outlook.com>
2020-10-30 18:21:05 +0300

    feat: Add config file for application.yml

--- src/main/java/com/example/twilio/TwilioConfig.java
+++ src/main/java/com/example/twilio/TwilioConfig.java
@@ -6,5 +6,35 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties("twilio")
 public class TwilioConfig {
+    private String accountSid;
+    private String authToken;
+    private String trialNum;
 
+    public TwilioConfig(){
+
+    }
+
+    public String getAccountSid() {
+        return accountSid;
+    }
+
+    public void setAccountSid(String accountSid) {
+        this.accountSid = accountSid;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public String getTrialNum() {
+        return trialNum;
+    }
+
+    public void setTrialNum(String trialNum) {
+        this.trialNum = trialNum;
+    }
 }
---
+++ src/main/java/com/example/twilio/TwilioInitializer.java
@@ -0,0 +1,7 @@
+package com.example.twilio;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TwilioInitializer {
+}
