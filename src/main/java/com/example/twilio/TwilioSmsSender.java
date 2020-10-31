ecaf52b11310054291032eda4afac6c1a5b77a57
isakulaksiz <isakulaksiz@outlook.com>
2020-10-31 14:28:20 +0300

    fix: Inject dependencies and check valid stat

--- src/main/java/com/example/twilio/TwilioSmsSender.java
+++ src/main/java/com/example/twilio/TwilioSmsSender.java
@@ -1,19 +1,36 @@
 package com.example.twilio;
 
 import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.rest.api.v2010.account.MessageCreator;
 import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TwilioSmsSender implements SmsSender {
+    private final TwilioConfig twilioConfig;
+
+    @Autowired
+    public TwilioSmsSender(TwilioConfig twilioConfig) {
+        this.twilioConfig = twilioConfig;
+    }
+
 
     @Override
     public void sendSMS(SmsRequest smsRequest) {
+        if(isPhoneNumValid(smsRequest.getPhoneNum())){
+            PhoneNumber to = new PhoneNumber(smsRequest.getPhoneNum());
+            PhoneNumber from = new PhoneNumber(twilioConfig.getTrialNum());
+            String message = smsRequest.getMessage();
+            MessageCreator creator = Message.creator(to, from, message);
+            creator.create();
+        }
+        else{
+            throw new IllegalArgumentException("Phone num "+smsRequest.getPhoneNum()+" is not valid number");
+        }
+    }
 
-        Message.creator(
-                new PhoneNumber(smsRequest.getPhoneNum()),
-                new PhoneNumber(""),
-                smsRequest.getMessage()
-        );
+    private boolean isPhoneNumValid(String phoneNum) {
+        return true; 
     }
 }
